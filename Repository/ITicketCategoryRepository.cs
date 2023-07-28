9d27acb82a4d3657891f3f2d9375cb7267711ce3
stefaniaOlaru <stefaniaolaru11@gmail.com>
2023-07-28 15:51:58 +0300

    update total price for order

---
+++ Repository/ITicketCategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using TicketManagementSystem.Models;
+
+namespace TicketManagementSystem.Repository
+{
+    public interface ITicketCategoryRepository
+    {
+        Task<TicketCategory> GetById(int id);
+    }
+}
---
+++ Repository/TicketCategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using TicketManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using TicketManagementSystem.Repository;
+
+namespace TicketManagementSystem.Repository
+{
+    public class TicketCategoryRepository : ITicketCategoryRepository
+    {
+        private readonly TicketManagementSystemContext _dbContext;
+
+        public TicketCategoryRepository()
+        {
+            _dbContext = new TicketManagementSystemContext();
+        }
+
+        public async Task<TicketCategory> GetById(int id)
+        {
+            var ticketCategory = await _dbContext.TicketCategories.Where(e => e.TicketCategoryId == id).FirstOrDefaultAsync();
+
+            return ticketCategory;
+        }
+    }
+}
